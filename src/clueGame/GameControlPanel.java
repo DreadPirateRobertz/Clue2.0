9fc0aa9bc0bde03c35af4c97d5444eaeefc42f75
DreadPirateRobertz <cdeal@mymail.mines.edu>
2021-04-05 12:53:51 -0600

    C21A-1 --> Setters are working as desired and comments have been added to this code

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -6,11 +6,74 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class GameControlPanel extends JPanel {
+    JTextField guessField;
+    JTextField guessResultField;
+    JTextField whoseTurnField;
+    JTextField dieNumber;
+
     public GameControlPanel()  {
+        guessField = new JTextField(20);
+        guessResultField = new JTextField( 20);
+        whoseTurnField = new JTextField(10);
+        dieNumber = new JTextField(5);
         setLayout(new BorderLayout());
         createLayout();
     }
 
+    private void createLayout(){   //This will add the two inner classes (which consists of nested panels) that were created below to the GameControlPanel
+        playerInfo playerInfo = new playerInfo();
+        add(playerInfo, BorderLayout.NORTH);
+        guessInfo guessInfo = new guessInfo();
+        add(guessInfo, BorderLayout.SOUTH);
+
+    }
+/* Creating two inner classes to hold all the guess & player information */
+    private class guessInfo extends JPanel {
+        guessInfo() {
+            JPanel guessBox = new JPanel();
+            guessBox.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
+            guessBox.add(guessField);
+            guessField.getText();
+            setLayout(new BorderLayout());
+            add(guessBox, BorderLayout.WEST);
+            JPanel guessResultBox = new JPanel();
+            guessResultBox.setBorder(new TitledBorder(new EtchedBorder(), "Guess Result"));
+            guessResultBox.add(guessResultField);
+            guessResultField.getText();
+            add(guessResultBox);
+        }
+    }
+    private class playerInfo extends JPanel { //Panels inside panels
+        playerInfo() {      //For the life of me I could not figure out how he centered "Whose Turn?" Still trying figure out GridLayout...
+            JPanel whoseTurnBox = new JPanel();
+            JLabel whoseTurn = new JLabel("Whose turn?");
+            whoseTurnBox.setLayout(new BorderLayout());
+            whoseTurnBox.add(whoseTurn, BorderLayout.NORTH);
+            whoseTurnBox.add(whoseTurnField, BorderLayout.SOUTH);
+            whoseTurnField.getText();
+            setLayout(new GridLayout());
+            add(whoseTurnBox, new GridLayout(0,1));
+            JPanel rollBox = new JPanel();
+            JLabel roll = new JLabel("Roll:");
+            rollBox.add(roll);
+            rollBox.add(dieNumber);
+            add(rollBox, new GridLayout(0, 2));
+            JButton accuseButton = new JButton("J'Accuse");
+            JButton nextButton = new JButton("NEXT");
+            add(accuseButton, new GridLayout(0,3));
+            add(nextButton, new GridLayout(0,4));
+        }
+    }
+    //Setters for updating all the fields
+    public void setWhoseTurn(Player playa){
+        whoseTurnField.setText(playa.getName());
+        whoseTurnField.setBackground(playa.getColor());
+    }
+    public void setGuess(String guess){ guessField.setText(guess); }
+    public void setGuessResult(String guessResult){ guessResultField.setText(guessResult); }
+    public void setDie(int roll){ dieNumber.setText(String.valueOf(roll)); }
+
+    //Main\\
     public static void main(String[] args) {
         GameControlPanel controlPanel = new GameControlPanel();
         JFrame frame = new JFrame();  // create the frame
@@ -18,66 +81,12 @@ public class GameControlPanel extends JPanel {
         frame.setSize(750, 120);  // size the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
         frame.setVisible(true); // make it visible
-        //frame.add(controlPanel, BorderLayout.SOUTH);
+
         // test filling in the data
-//        panel.setTurn(new ComputerPlayer( "Col. Mustard", 0, 0, "orange"), 5);
-//        panel.setGuess( "I have no guess!");
-//        panel.setGuessResult( "So you have nothing?");;
+        controlPanel.setWhoseTurn(new Computer("Larry", Color.green, "Galley"));
+        controlPanel.setGuess( ":)");
+        controlPanel.setGuessResult( ":):):)");
+        controlPanel.setDie(3);
     }
-    private void createLayout(){
-        playerInformation playerInformation = new playerInformation();
-
-        add(playerInformation, BorderLayout.NORTH);
-        guessInformation guessInformation = new guessInformation();
-        add(guessInformation, BorderLayout.SOUTH);
-
-    }
-
-    private class guessInformation extends JPanel {
-        guessInformation() {
-            JPanel guessBox = new JPanel();
-            guessBox.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
-            JTextField guessField = new JTextField("I have no guess", 20);
-
-            guessBox.add(guessField);
-            setLayout(new BorderLayout());
-            add(guessBox, BorderLayout.WEST);
-
-
-            JPanel guessResultBox = new JPanel();
-            guessResultBox.setBorder(new TitledBorder(new EtchedBorder(), "Guess Result"));
-            JTextField guessResultField = new JTextField("So you have nothing", 20);
-            guessResultBox.add(guessResultField);
-            add(guessResultBox);
-
-        }
-    }
-    private class playerInformation extends JPanel {
-        playerInformation() {
-            JPanel whoseTurnBox = new JPanel();
-            JLabel whoseTurn = new JLabel("Whose turn?");
-            JTextField field = new JTextField(10);
-            whoseTurnBox.setLayout(new BorderLayout());
-            whoseTurnBox.add(whoseTurn, BorderLayout.NORTH);
-            whoseTurnBox.add(field, BorderLayout.SOUTH);
-            setLayout(new GridLayout());
-            add(whoseTurnBox, new GridLayout(0,1));
-
-
-            JPanel rollBox = new JPanel();
-            JLabel roll = new JLabel("Roll:");
-            JTextField dieNumber = new JTextField(5);
-//            rollBox.setLayout(new BorderLayout());
-            rollBox.add(roll);
-            rollBox.add(dieNumber);
-            add(rollBox, new GridLayout(0, 2));
-
-            JButton accuseButton = new JButton("Make Accusation");
-            JButton nextButton = new JButton("NEXT");
-            add(accuseButton, new GridLayout(0,3));
-            add(nextButton, new GridLayout(0,4));
-        }
-    }
-
 }
 
